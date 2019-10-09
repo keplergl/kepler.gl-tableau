999abd2eb0ef90d8bfe55f3a8c1cb9863b5578cb
Chris DeMartini <chris@datablick.com>
2019-10-08 22:48:22 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/keplergl/kepler.gl-tableau/precache-manifest.d76a59661701a0aec4687be8a29c76fe.js"
+  "/precache-manifest.6cf925e07b27e47531d9c0bc554b240d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/keplergl/kepler.gl-tableau/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
